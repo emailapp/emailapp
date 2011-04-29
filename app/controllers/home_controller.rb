7303799adcfed49e972e2c74f0beb1d80425e046
Cheng-Hsin Weng <kkmimic1984@gmail.com>
2011-04-29 13:54:31 -0500

    only show future events on front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    #@events = Event.find(:all).sort_by {|e| change_date_format(e.schedule_time)}
-    @events = Event.all
+    #@events = Event.find(:all).sort_by {|e| change_date_format(e.schedule_time)} #string sorting
+    @events = Event.find(:all, :order => "schedule_time DESC", :conditions => ["schedule_time > ?", Date.today])
     @contacts = Contact.all
   end
 
