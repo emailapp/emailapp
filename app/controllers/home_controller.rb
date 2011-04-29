e2df29a3cdd665ac2581ff48b500d5dd0da3ae28
Cheng-Hsin Weng <kkmimic1984@gmail.com>
2011-04-29 13:29:29 -0500

    not using the change_date_format method for sorting for front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.find(:all).sort_by {|e| change_date_format(e.schedule_time)}
+    #@events = Event.find(:all).sort_by {|e| change_date_format(e.schedule_time)}
+    @events = Event.all
     @contacts = Contact.all
   end
 
