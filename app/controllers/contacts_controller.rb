f1b311955bc4c54c46f75e367f00a9fe37707f84
Cheng-Hsin Weng <kkmimic1984@gmail.com>
2011-04-29 15:40:59 -0500

    sorted contacts by first name and last name

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
   # GET /contacts
   # GET /contacts.xml
   def index
-    @contacts = Contact.all
+    @contacts = Contact.find(:all, :order => "first ASC, last ASC")
 
     respond_to do |format|
       format.html # index.html.erb
