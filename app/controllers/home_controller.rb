b6bd11db31c5793a3af9135e2d5a9af58c5a3a54
Cheng-Hsin Weng <kkmimic1984@gmail.com>
2011-04-29 12:54:02 -0500

    sorting events for front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.find(:all).sort_by {|e| change_date_format(e.schedule_time)}
     @contacts = Contact.all
   end
 
