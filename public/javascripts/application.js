32586b32eb778e0c21208702d4ebf70ac7c700e4
Cong Sun <cong.c.sun@gmail.com>
2011-04-30 13:26:38 -0500

    finished adding input data validation feature & date input datepicker feature

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$function () {
-  $("#schedule_time").datepicker();
+jQuery(function () {
+  jQuery("#datepicker").datepicker();
   });
