da229d080053a8027df831f8e0e7738b64edcc04
Cheng-Hsin Weng <kkmimic1984@gmail.com>
2011-04-30 21:17:29 -0500

    changed validatation methods and fields for birday

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,15 @@ class Contact < ActiveRecord::Base
                     :length => {:minimum => 3, :maximum => 254},
                     :uniqueness => true,
                     :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
-  validates :birthday, :format => {:with => /\d{2}\/\d{2}\/\d{4}/, :message => "^Date must be in the following format: mm/dd/yyyy"}
+  #validates :birthday, :format => {:with => /\d{2}\/\d{2}\/\d{4}/, :message => "^Date must be in the following format: mm/dd/yyyy"}
+
+  validate :birthday_is_valid
+ 
+  def birthday_is_valid
+    if !birthday_before_type_cast.blank? && birthday.blank?
+       errors.add(:birthday, "must be in the mm/dd format")
+    end
+  end
   
 
   def full_name
--- app/models/event.rb
+++ app/models/event.rb
@@ -8,6 +8,16 @@ class Event < ActiveRecord::Base
   end
 
   validates :name, :subject, :message, :presence => true
-  validates :schedule_time, :format => {:with => /\d{2}\/\d{2}\/\d{4}/, :message => "^Date must be in the following format: mm/dd/yyyy"}
+  #validates :schedule_time, :format => {:with => /\d{2}\/\d{2}\/\d{4}/, :message => "must be in the mm/dd/yyyy format"}
+  
+  validate :schedule_time_is_valid
+ 
+  def schedule_time_is_valid
+    if schedule_time_before_type_cast.blank?
+      errors.add(:schedule_time, "can't be blank")
+    elsif schedule_time.blank?
+      errors.add(:schedule_time, "must be in the mm/dd/yyyy format")
+    end
+  end
 
 end
