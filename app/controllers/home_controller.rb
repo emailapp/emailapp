0f85513911266203939f9cfd0c000c750fdea9c6
Cheng-Hsin Weng <kkmimic1984@gmail.com>
2011-05-02 13:50:37 -0500

    added shadow for navigation and display the events which are happening today and haven't sent

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     #@events = Event.find(:all).sort_by {|e| change_date_format(e.schedule_time)} #string sorting
-    @events = Event.find(:all, :order => "schedule_time ASC", :conditions => ["schedule_time > ?", Date.today], :limit => 20)
+    @events = Event.find(:all, :order => "schedule_time ASC", :conditions => ["schedule_time >= ? AND status = 0", Date.today], :limit => 20)
     @contacts = Contact.find(:all, :order => "created_at DESC", :limit => 20)
     @smtp_info = SmtpInfo.find(:first)
   end
