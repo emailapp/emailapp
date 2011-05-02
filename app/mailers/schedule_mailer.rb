1a51eb726da5f5178388fbe4256ad122b0cd6ac2
Cheng-Hsin Weng <kkmimic1984@gmail.com>
2011-05-01 19:34:49 -0500

    added greeting for mail content

--- app/mailers/schedule_mailer.rb
+++ app/mailers/schedule_mailer.rb
@@ -3,8 +3,8 @@ class ScheduleMailer < ActionMailer::Base
   def send_event_mails(mail_info, event, from)
 
     mail(:to => mail_info.email, :subject => event.subject, :from => from) do |format|
-      format.text { render :text => event.message }
-      format.html { render :text => event.message }
+      format.text { render :text => "Dear #{mail_info.first},\n#{event.message}" }
+      #format.html { render :text => event.message }
           
     end
 
