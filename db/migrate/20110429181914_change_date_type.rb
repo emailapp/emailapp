17749052c57cef85f42e910c7c796561e6b8e760
Cong Sun <cong.c.sun@gmail.com>
2011-05-14 10:16:18 -0500

    Just pushed Cong branch to heroku using 'git push -f heroku cong:refs/heads/master' but after running 'heroku rake db:migrate' there was still the error. It seems to be because I omitted one 'End' in the ChangeDateType file.

--- db/migrate/20110429181914_change_date_type.rb
+++ db/migrate/20110429181914_change_date_type.rb
@@ -8,6 +8,8 @@ class ChangeDateType < ActiveRecord::Migration
     Event.reset_collumn_information
     Event.find_each { |c| c.update_attribute(:schedule_time, c.schedule_time_string) }
     delete_column :events, :schedule_time_string
+
+    end
     
   #end
   #def self.down
