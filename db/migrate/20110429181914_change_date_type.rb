ea002e312bdfc617b7426a7c7c416e57c83121f7
Cong Sun <cong.c.sun@gmail.com>
2011-05-14 10:08:25 -0500

    fixed the round-border issue with Firefox; trying to fix the heroku migrate issue with the change column codes in ChangeDateType

--- db/migrate/20110429181914_change_date_type.rb
+++ db/migrate/20110429181914_change_date_type.rb
@@ -1,11 +1,18 @@
 class ChangeDateType < ActiveRecord::Migration
   def self.up
-    change_column :events, :schedule_time, :date
-    change_column :contacts, :birthday, :date
-  end
-
-  def self.down
-    change_column :events, :schedule_time, :string
-    change_column :contacts, :birthday, :string
-  end
+    #change_column :events, :schedule_time, :date
+    #change_column :contacts, :birthday, :date
+    rename_column :events, :schedule_time, :schedule_time_string
+    create_column :events, :schedule_time, :date
+    
+    Event.reset_collumn_information
+    Event.find_each { |c| c.update_attribute(:schedule_time, c.schedule_time_string) }
+    delete_column :events, :schedule_time_string
+    
+  #end
+  #def self.down
+  #  change_column :events, :schedule_time, :string
+  #  change_column :contacts, :birthday, :string
+  #end
+  
 end
